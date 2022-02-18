b44a7d12fd8b5c41c5dbf57cd7d4cc411e4c82eb
Dosu333 <vladamir1865@gmail.com>
2022-02-18 14:00:53 +0100

    fixed resend token bug

--- src/components/ResendToken/ResendToken.jsx
+++ src/components/ResendToken/ResendToken.jsx
@@ -10,7 +10,7 @@ const ResendToken = () => {
     const [emailResend, setEmailResend] = useState({
         email: ""
     })
-    const [loaded,setLoaded] = useState(false)
+    // const [loaded,setLoaded] = useState(false)
     const [error, setError] = useState("")
     
     const baseURL = "https://api.boxin.ng/api/v1"
@@ -34,9 +34,21 @@ const ResendToken = () => {
             axios.post(`${baseURL}/auth/users/resend-token/`, emailResend)
             .then(res => {
                 console.log(res.data)
+                if (res.data.success) {
+                    history.push('/verifytoken')
+                    alert(res.data.message)
+                } else {
+                    alert('You are verified already')
+                    history.push('/signin')
+                }
+                
             })
-            .then(setLoaded(true))
-            .catch(err => {console.log(err)})
+            // .then(setLoaded(true))
+            .catch(err => {
+                console.log(err)
+                setError('User with this email does not exist')
+                alert('User with this email does not exist')
+            })
             
             // res.data.success ? console.log("Success") : console.log('Failure');
             // console.log(loaded);
@@ -44,11 +56,11 @@ const ResendToken = () => {
 
         postResentEmailTokenToDB()
         // console.log(loaded)
-        loaded ? history.push('/verifytoken') : null;
+        // loaded ? history.push('/verifytoken') : null;
 
     }
 
-    console.log(loaded);
+    // console.log(loaded);
 
     return ( 
         <div className="resendToken">
